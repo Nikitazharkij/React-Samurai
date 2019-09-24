941fd1a4b8b3d3f2145ae4805b61245084f753f2
NikitaZharkij <kakdelanik@mail.ru>
2019-09-24 14:35:18 +0300

    Lesson 73

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   withCredentials: true,
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
   headers: {
-    "API-KEY": "28651c2a-66cc-4756-a2f2-fd84b84e262a"
+    "API-KEY": "e08cf415-a88a-4a57-b43d-83001a3eb492"
   }
 });
 
@@ -22,7 +22,20 @@ export const usersAPI = {
     return instance.delete(`follow/${userId}`)
   },
   getProfile(userId) {
+    console.warn('Obsolete method. Please profileAPI object.');
+    return profileAPI.getProfile(userId);
+  }
+}
+
+export const profileAPI = {
+  getProfile(userId) {
     return instance.get(`profile/` + userId);
+  },
+  getStatus(userId) {
+    return instance.get(`profile/status/` + userId);
+  },
+  updateStatus(status) {
+    return instance.put(`profile/status`, { status: status});
   }
 }
 
--- src/components/Profile/ProfileInfo/ProfileStatus.js
+++ src/components/Profile/ProfileInfo/ProfileStatus.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 class ProfileStatus extends React.Component {
+
     state = {
-        editMode: false
+        editMode: false,
+        status: this.props.status
     };
 
     activateEditMode = () => {
@@ -11,9 +13,16 @@ class ProfileStatus extends React.Component {
         });
     };
 
-    deactivateEditMode() {
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
+        });
+        this.props.updateStatus(this.state.status);
+    }
+
+    onStatusChange = (e) => {
+        this.setState({
+            status: e.currentTarget.value
         });
     }
 
@@ -22,12 +31,12 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status || '-----' }</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                        <input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status}/>
                     </div>
                 }
             </div>
